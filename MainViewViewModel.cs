23d4b549871c2c9914ef0a87e23877aa8ab4fc28
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-03 09:52:20 +0300

    Добавьте файлы проекта.

---
+++ MainViewViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIChangeWallType
+{
+    class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SaveCommand { get; }
+        public List<Element> PickedObjects { get; } = new List<Element>();
+        public List<WallType> WallTypes { get; } = new List<WallType>();
+        public WallType SelectedWallType { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            PickedObjects = SelectionUtils.PickObjects(commandData);
+            WallTypes = GetWallTypes();
+        }
+
+        private List<WallType> GetWallTypes()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var walls = new FilteredElementCollector(doc)
+                        .OfClass(typeof(WallType))
+                        .Cast<WallType>()
+                        .ToList();
+
+            return walls;
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (WallTypes.Count == 0 || SelectedWallType == null)
+                return;
+
+            using (var ts = new Transaction(doc, "Set wall type"))
+            {
+                ts.Start();
+                foreach (var element in PickedObjects)
+                {
+                    if (element is Wall)
+                    {
+                        Wall wall = element as Wall;
+                        wall.WallType = SelectedWallType;
+                    }
+                }
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
